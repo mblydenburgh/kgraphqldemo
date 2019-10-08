2702961b0d7260e66bc63df5e675092f5c5b39b8
N0364359 <michael.blydenburgh@libertymutual.com>
2019-10-08 15:36:50 -0400

    csv data imported correctly

--- src/main/kotlin/com/blydenburgh/kgraphqldemo/service/TeamImporter.kt
+++ src/main/kotlin/com/blydenburgh/kgraphqldemo/service/TeamImporter.kt
@@ -9,32 +9,30 @@ import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 
 @Component
-class TeamImporter: CSVDataImporter() {
+class TeamImporter : CSVDataImporter() {
     @Value("classpath:\${app.file.teams}")
     lateinit var teamsResource: Resource
 
     @Autowired
     lateinit var teamRepository: TeamRepository
 
-    override fun import() {
-         fun import() = importFromCsv(teamsResource.inputStream) { row ->
-             teamRepository.save(Team(
-                 id = row[0],
-                 minutes = row[1].toDouble(),
-                 possessionsFor = row[2].toInt(),
-                 possessionsOpp = row[3].toInt(),
-                 pointsFor = row[4].toInt(),
-                 pointsOpp = row[5].toInt(),
-                 offRtg = row[6].toDouble(),
-                 defRtg = row[7].toDouble(),
-                 overallRtg = row[8].toDouble(),
-                 oRebFor = row[9].toInt(),
-                 oRebOpp = row[10].toInt(),
-                 dRebFor = row[11].toInt(),
-                 dRebOpp = row[12].toInt(),
-                 oRebRate = row[13].toDouble(),
-                 dRebRate = row[14].toDouble()
-             ))
-         }
+    override fun import() = importFromCsv(teamsResource.inputStream) { row ->
+        teamRepository.save(Team(
+            id = row[0],
+            minutes = row[1].toDouble(),
+            possessionsFor = row[2].toInt(),
+            possessionsOpp = row[3].toInt(),
+            pointsFor = row[4].toInt(),
+            pointsOpp = row[5].toInt(),
+            offRtg = row[6].toDouble(),
+            defRtg = row[7].toDouble(),
+            overallRtg = row[8].toDouble(),
+            oRebFor = row[9].toInt(),
+            oRebOpp = row[10].toInt(),
+            dRebFor = row[11].toInt(),
+            dRebOpp = row[12].toInt(),
+            oRebRate = row[13].toDouble(),
+            dRebRate = row[14].toDouble()
+        ))
     }
 }
